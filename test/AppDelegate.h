e414e8dffba6a5169e0517f5630bd766cfa09e78
henghadawang <867561903@qq.com>
2017-01-19 10:09:18 +0800

    init

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by LuWei on 17/1/19.
+//  Copyright © 2017年 LuWei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by LuWei on 17/1/19.
+//  Copyright © 2017年 LuWei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
